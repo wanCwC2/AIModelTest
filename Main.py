8982812fda66300b4f5a57dbc3c38af8c35fdbac
wanCwC2 <WanPcwc0630@gmail.com>
2022-04-30 13:05:16 +0800

    Read data and Split it

--- Main.py
+++ Main.py
@@ -1,7 +1,21 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 30 11:47:57 2022
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import math
+from sklearn.model_selection import train_test_split
 
-@author: idea3c
-"""
+#Read data & Split it
+df = pd.read_csv("data/train.csv")
+#print(pd.DataFrame(df).head(3)) #The first three data
+#print(df.columns) #data Index
+'''
+data Index
+['datetime', 'season', 'holiday', 'workingday', 'weather', 'temp', 'atemp', 'humidity', 'windspeed', 'casual', 'registered', 'count'],
+      dtype='object')
+'''
+x_df = pd.DataFrame(df, columns = ['season', 'holiday', 'workingday', 'weather', 'temp', 'atemp', 'humidity', 'windspeed', 'casual', 'registered'])
+y_df = pd.DataFrame(df, columns = ['count'])
+x_train, x_valid, y_train, y_valid = train_test_split(x_df, y_df, train_size=0.6, random_state=0)
+x_valid, x_test, y_valid, y_test = train_test_split(x_valid, y_valid, train_size=0.5, random_state=0)
+#print(x_train, x_valid, x_test, y_train, y_valid, y_test)
